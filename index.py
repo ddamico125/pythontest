ace113f733666a9007f5f15eb30001f982c27adf
ddamico125 <ddamico.125@gmail.com>
2018-07-16 21:57:35 -0400

    Add things to make get req on dynamodb

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import json
 import datetime
+import boto3
 
 
 def handler(event, context):
@@ -10,3 +11,24 @@ def handler(event, context):
     return {'statusCode': 200,
             'body': json.dumps(data),
             'headers': {'Content-Type': 'application/json'}}
+
+
+dynamodb = boto3.resource('dynamodb')
+
+def get(event, context):
+  table = dynamodb.Table('test_user_tabel')
+
+    # fetch todo from the database
+    result = table.get_item(
+      Key={
+      'id': event['pathParameters']['id']
+      }
+      )
+
+    # create a response
+    response = {
+    "statusCode": 200,
+    "body": "body",
+    }
+
+    return response
